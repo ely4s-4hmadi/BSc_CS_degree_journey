391e08311f88f7f838fb7bc00b2b87ca5a9259ea
Mohammad Elyas Ahmadi <111071345+ely4s-4hmadi@users.noreply.github.com>
2026-07-02 01:59:44 +0100

    Add files via upload

---
+++ year1/semester1/COMP101/labs/Lab-03_bugged.py
@@ -0,0 +1,11 @@
+#COMP101_Lab-03_bugged.py:  
+# i/p two integers and perform subtraction 
+input
+num1 = (input("Enter num1: ")
+num2 = int input("Enter num2 : ))
+
+#calculation
+return == (num1+num2)
+
+#output
+print (return)
---
+++ year1/semester1/COMP101/labs/lab01_sequence.py
@@ -0,0 +1,30 @@
+#lab01_ex310_sequence.py
+#KD - Tutor: Open date
+#Example of sequential code in action
+
+print ('This is an example of sequential code')
+input ('Press return to continue ...')
+print ('There are no selection or iteration statements in it')
+print ('So it works okay but has limited functionality')
+print()
+print ('Sequential code executes one line at a time in the order in which it was writen')
+input ('Press Return to continue ...')
+input ('The next lines of code will output a menu - press Return to continue ...')
+print()             #output a blank line betwen the messages above and the menu
+print ('Main Menu')
+print ('---------')
+print ('D: Division')
+print ('M: Multiplication')
+print ('A: Addition')
+print ('S: Subtraction')
+print ('X: Exit Menu: ')
+print()             #output a blank line between menu and the prompt
+print ('Enter an option D,M,A,S or X to exit: ')
+print()
+
+#below, we have given the user a message
+
+input ('You can see a blank line between the menu and the prompt')
+
+#but we haven't told them what to do to carry on (they have to press Return)
+input('Holding the screen - press Return to end the program')
